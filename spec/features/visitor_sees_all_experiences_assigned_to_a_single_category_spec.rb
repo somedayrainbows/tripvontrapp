40b58a615a164f3654aab58a477bbe5b15782024
NatBee <nataliesbarron@gmail.com>
2017-04-10 21:03:50 -0600

    Worked through resolving some of the errors Test for this iteration is still failing

--- spec/features/visitor_sees_all_experiences_assigned_to_a_single_category_spec.rb
+++ spec/features/visitor_sees_all_experiences_assigned_to_a_single_category_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-describe "User sees all experiences in a category" do
-  scenario "user sees all experiences" do
+describe "User sees all experiences" do
+  scenario "when they visit the category show" do
 
     arts = create(:category)
     art_walk = create_list(:experience, 2)
 
-    visit category_path
+    visit "/categories/arts"
 
-    expect(page).to have_content(arts.name1)
+    expect(page).to have_content(arts.name)
     expect(page).to have_content(art_walk.title1)
     expect(page).to have_content(art_walk.title2)
     expect(page).to have_button("Add to cart")
