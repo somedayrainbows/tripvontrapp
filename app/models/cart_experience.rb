634531ae7271d899bc6d343bca509a06008c99e3
stovermc <stovermc@gmail.com>
2017-04-12 15:42:37 -0600

    complete cart experience with simpledelegator

--- app/models/cart_experience.rb
+++ app/models/cart_experience.rb
@@ -1,11 +1,24 @@
-class CartExperience < SimpleDelgator
-  # extend Forwardable
-  #
-  # def_delegators :@experience, :title, :description, :cost, :city, :image_path
+class CartExperience < SimpleDelegator
 
   def initialize(id, quantity)
     @id = id
     @quantity = quantity
-    super Experience.find_by(@id)
+    super Experience.find(@id)
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+# extend Forwardable
+#
+# def_delegators :@experience, :title, :description, :cost, :city, :image_path
+# delegate :Experience, :title
