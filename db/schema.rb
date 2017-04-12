efcb90367bf1b9f97da85923e29526e0d0279ffb
NatBee <nataliesbarron@gmail.com>
2017-04-12 15:42:04 -0600

    Completed User Creation
    Completed user creation code for authentication.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170411223910) do
+ActiveRecord::Schema.define(version: 20170412194400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,13 @@ ActiveRecord::Schema.define(version: 20170411223910) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "image_path"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string  "name"
+    t.string  "email"
+    t.string  "password_digest"
+    t.integer "role",            default: 0
   end
 
   add_foreign_key "experience_categories", "categories"
--- spec/features/users/user_can_create_a_new_account_spec.rb
+++ spec/features/users/user_can_create_a_new_account_spec.rb
@@ -1,24 +1,24 @@
 require 'rails_helper'
 
 describe 'User visits root' do
-  scenario 'they are redirected to login page and see the form to create a new account' do
+  scenario 'they click a link to login and are redirected to a login page where they see the form to create a new account' do
     # As a visitor
     # When I visit "/"
-    visit "/"
+# visit "/"
     # Then I should see a link for "Login"
     # And when I click "Login"
-    click_link "Login"
+# click_link "Login"
     # And I should be on the "/login" page
     visit "/login"
 
     expect(current_path).to eq(login_path)
+
     # I should see a place to insert my credentials to login
     # And I should see a link to "Create Account"
-    expect(page).to have_content("Create Account")
-    expect(page).to have_content("Name:")
-    expect(page).to have_content("Email:")
-    expect(page).to have_content("Password:")
-    expect(page).to have_content("Create Account")
+    expect(page).to have_content("Name")
+    expect(page).to have_content("Email")
+    expect(page).to have_content("Password")
+    expect(page).to have_button("Create Account")
   end
 end
 
@@ -28,19 +28,17 @@ describe "user visits login page" do
     # When I visit "/login
     visit "/login"
     # And when I click link "Create Account"
-    click_on "Create Account"
     # And I fill in my desired credentials
-    within("form") do
-      fill_in "user[name]", with: "Natbot"
-      fill_in "user[email]", with: "nat@nat.com"
-      fill_in "user[password]" with: "password"
-      click_on "Submit"
-    end
+      fill_in "Name", with: "Natbot"
+      fill_in "Email", with: "nat@nat.com"
+      fill_in "Password", with: "password"
+      click_on "Create Account"
+
+
     # And I submit my information
     # Then my current page should be "/dashboard"
-    visit "/dashboard"
     # And I should see a message in the navbar that says "Logged in as SOME_USER"
-    expect(current_path).to eq(user_path(user))
+    expect(current_path).to eq(user_path(@user))
     expect(page).to have_content("Logged in as Natbot. Welcome!")
     # And I should see my profile information
     expect(page).to have_content("Profile Information")
