3b9521bb4a892d08ad17aafd6a762d408e5bb7cc
stovermc <stovermc@gmail.com>
2017-04-11 18:35:14 -0600

    add update action in carts controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,17 +3,20 @@ class CartsController < ApplicationController
 
   def create
     experience = Experience.find(params[:experience_id])
-    @cart.add_experience(experience.id)
     session[:cart] = @cart.contents
     flash[:notice] = "#{pluralize(@cart.count_of(experience.id), experience.title)} added to your cart."
     redirect_to experiences_path
   end
 
-  def index
+  def show
     @cart_experiences = @cart.cart_experiences
   end
 
   def update
-
+    experience = Experience.find(params[:experience_id])
+    @cart.add_experience(experience.id)
+    session[:cart] = @cart.contents
+    flash[:notice] = "#{pluralize(@cart.count_of(experience.id), experience.title)} added to your cart."
+    redirect_to cart_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-  get '/cart', to: 'carts#index'
-  post '/cart', to: 'carts#index'
+  # get '/cart', to: 'carts#index'
+  # post '/cart', to: 'carts#index'
 
   resources :categories, only: [:index, :show]
   resources :experiences, only: [:index]
 
-  resources :carts
+  resource :cart
 end
