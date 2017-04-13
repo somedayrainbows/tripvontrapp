85e91d9d85b90de51fa72f3ae9803aeee1c00616
NatBee <nataliesbarron@gmail.com>
2017-04-12 19:10:19 -0600

    Authentication complete
    Logout functionality of authentication is complete.
    Feature tests for authentication are passing.
    User story 7 Authenticated User is complete.

--- spec/features/users/user_can_create_a_new_account_spec.rb
+++ spec/features/users/user_can_create_a_new_account_spec.rb
@@ -33,16 +33,16 @@ describe "user visits login page" do
 
     expect(current_path).to eq(new_user_path)
     # And I fill in my desired credentials
-      fill_in "Name", with: "Natbot"
-      fill_in "Email", with: "nat@nat.com"
-      fill_in "Password", with: "password"
-      click_on "Create Account"
+    fill_in "Name", with: "Natbot"
+    fill_in "Email", with: "nat@nat.com"
+    fill_in "Password", with: "password"
+    click_on "Create Account"
 
 
     # And I submit my information
     # Then my current page should be "/dashboard"
     # And I should see a message in the navbar that says "Logged in as SOME_USER"
-    expect(current_path).to eq(user_path(user))
+    expect(current_path).to eq(user_path(User.last))
     expect(page).to have_content("Logged in as Natbot. Welcome!")
     # And I should see my profile information
     expect(page).to have_content("Profile Information")
@@ -51,6 +51,10 @@ describe "user visits login page" do
     # And I should not see a link for "Login"
     expect(page).to_not have_content("Login")
     # And I should see a link for "Logout"
-    expect(page).to have_content("Logout")
+    expect(page).to have_button("Logout")
+
+    click_on "Logout"
+
+    expect(current_path).to eq(login_path)
   end
 end
