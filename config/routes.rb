cc6592ab69fba0bdfc363f545b23874924c8e4c1
somedayrainbows <ebassity@gmail.com>
2017-04-13 13:05:08 -0600

    style homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'dashboard#index'
+  root 'home#index'
 
   # get '/cart', to: 'carts#index'
   # post '/cart', to: 'carts#index'
---
+++ spec/features/home/visitors_can_view_a_home_page_spec.rb
@@ -0,0 +1,8 @@
+# require 'rails_helper'
+#
+# RSpec.feature "visitor lands on root home page" do
+#   context ""
+# # as a visitor, when visiting the site root
+# # i see promoted content on trips available
+# # and i can
+# #
