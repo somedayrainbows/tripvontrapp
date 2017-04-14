bdda0c35b0cbecbc293ec89648adb94c4571eb7c
slague <stephanielague@gmail.com>
2017-04-13 19:01:05 -0600

    Clean up experience index test
    * sent it to the correct path

--- spec/features/visitor_views_all_experiences_spec.rb
+++ spec/features/visitor_views_all_experiences_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 
 describe "visitor views all experiences" do
-  xscenario "when they visit the experience index" do
+  scenario "when they visit the experience index" do
 
-      experience = build(:experience)
-      visit categories_path
+      experience = create(:experience)
+      visit experiences_path
 
-      expect(page).to have_content(experience.title)
+      expect(page).to have_content(experience.title)   
       expect(page).to have_content(25)
       expect(page).to have_button("Add to cart")
 
