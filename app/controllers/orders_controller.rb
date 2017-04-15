f0c2e1c04b8e7a19b17df8d521ecbd24ab712aa9
stovermc <stovermc@gmail.com>
2017-04-15 15:15:11 -0600

    complete checkout functionality
    * added create action and cart_reset method to 
    Controller. 
    
    * added method to order model for adding experiences
    to a single order.
    
    * modified checkout button to send a post to the 
    orders#action.
    
    * added flash message to create action in orders
    Controller.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,7 @@
 class OrdersController < ApplicationController
 
+  # after_create :cart_reset
+
   def index
     user = current_user
     if current_user
@@ -9,11 +11,23 @@ class OrdersController < ApplicationController
     end
   end
 
-
+def create
+  @order = Order.create(total_price: @cart.total, user_id: current_user.id)
+  @order.add_experience_to_order(@cart)
+  flash[:success] = "Order was successfully placed."
+  cart_reset
+  redirect_to orders_path
+end
 
   def show
     user = current_user
     @order = user.orders.find(params[:id])
   end
 
+
+private
+
+  def cart_reset
+    session[:cart] = nil
+  end
 end
