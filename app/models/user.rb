2b1c84a0b3476b89c412cddd04d53f3272b40e59
slague <stephanielague@gmail.com>
2017-04-19 21:24:32 -0600

    validate for name, email, password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   has_secure_password
   has_many :orders
 
+  validates :name, presence: true
   validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
 
   enum role: %w(default admin)
 
